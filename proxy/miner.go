a7a5cb7d41b7c55c12329caea6918c71bcfeb84f
Just Another Multi Pool Source <76232556+J-A-M-P-S@users.noreply.github.com>
2021-08-11 16:47:21 +0200

    Update miner.go

--- proxy/miner.go
+++ proxy/miner.go
@@ -16,104 +16,6 @@ var ecip1099FBlockMordor uint64 = 2520000   // mordor
 
 var hasher *etchash.Etchash = nil
 
-//// NiceHash share processing
-func (s *ProxyServer) processShareNH(login, id, ip string, t *BlockTemplate, params[]string) (bool, bool) {
-	if hasher == nil {
-		if s.config.Network == "classic" {
-			hasher = etchash.New(&ecip1099FBlockClassic, nil)
-		} else if s.config.Network == "mordor" {
-			hasher = etchash.New(&ecip1099FBlockMordor, nil)
-		} else {
-			// unknown network
-			log.Printf("Unknown network configuration %s", s.config.Network)
-			return false, false
-		}
-	}
-	nonceHex := params[0]
-	nonce, _ := strconv.ParseUint(strings.Replace(nonceHex, "0x", "", -1), 16, 64)
-	hashNoNonce := common.HexToHash(params[2])
-
-	shareDiffFloat, mixDigest := hasher.GetShareDiff(t.Height, hashNoNonce, nonce)
-
-	if shareDiffFloat < 0.0001 {
-		log.Printf("share difficulty too low, %f < %d, from %v@%v", shareDiffFloat, t.Difficulty, login, ip)
-		return false, false
-	}
-
-	shareDiffFloat = shareDiffFloat * 0.98
-
-	shareDiff_big := util.DiffFloatToDiffInt(shareDiffFloat)
-
-	shareDiff := shareDiff_big.Int64()
-
-	h, ok := t.headers[hashNoNonce.Hex()]
-	if !ok {
-		log.Printf("Stale share from %v@%v", login, ip)
-		return false, false
-	}
-
-	share := Block{
-		number:      h.height,
-		hashNoNonce: common.HexToHash(hashNoNonce.Hex()),
-		difficulty:  big.NewInt(shareDiff),
-		nonce:       nonce,
-		mixDigest:   common.HexToHash(mixDigest.Hex()),
-	}
-
-	block := Block{
-		number:      h.height,
-		hashNoNonce: common.HexToHash(hashNoNonce.Hex()),
-		difficulty:  h.diff,
-		nonce:       nonce,
-		mixDigest:   common.HexToHash(mixDigest.Hex()),
-	}
-
-	if !hasher.Verify(share) {
-		log.Println("!hasher.Verify(share)")
-		return false, false
-	}
-
-	if hasher.Verify(block) {
-		submit_params := []string{
-			nonceHex,
-			hashNoNonce.Hex(),
-			mixDigest.Hex(),
-		}
-
-		ok, err := s.rpc().SubmitBlock(submit_params)
-
-		if err != nil {
-			log.Printf("Block submission failure at height %v for %v: %v", h.height, t.Header, err)
-		} else if !ok {
-			log.Printf("Block rejected at height %v for %v", h.height, t.Header)
-			return false, false
-		} else {
-			s.fetchBlockTemplate()
-			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, h.diff.Int64(), h.height, s.hashrateExpiration)
-			if exist {
-				return true, false
-			}
-			if err != nil {
-				log.Println("Failed to insert block candidate into backend:", err)
-			} else {
-				log.Printf("Inserted block %v to backend", h.height)
-			}
-			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
-		}
-	} else {
-		exist, err := s.backend.WriteShare(login, id, params, shareDiff, h.height, s.hashrateExpiration)
-		if exist {
-			return true, false
-		}
-		if err != nil {
-			log.Println("Failed to insert share data into backend:", err)
-		}
-	}
-	return false, true
-
-}
-
-//// Regular Stratum share processing
 func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, params []string) (bool, bool) {
 	if hasher == nil {
 		if s.config.Network == "classic" {
@@ -135,6 +37,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 	h, ok := t.headers[hashNoNonce]
 	if !ok {
 		log.Printf("Stale share from %v@%v", login, ip)
+		s.backend.WriteWorkerShareStatus(login, id, false, true, false)
 		return false, false
 	}
 
@@ -155,6 +58,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 	}
 
 	if !hasher.Verify(share) {
+		s.backend.WriteWorkerShareStatus(login, id, false, false, true)
 		return false, false
 	}
 
@@ -187,5 +91,6 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			log.Println("Failed to insert share data into backend:", err)
 		}
 	}
+	s.backend.WriteWorkerShareStatus(login, id, true, false, false)
 	return false, true
 }
